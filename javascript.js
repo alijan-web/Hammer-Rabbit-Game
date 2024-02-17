12d94c1a7cdf0c887f8745b47076e1edc5aa4959
Ali Jan <alijaan.web@gmail.com>
2024-02-17 13:49:35 +0500

    js complete and projact complete

--- javascript.js
+++ javascript.js
@@ -1,4 +1,48 @@
 var cursor = document.querySelector(".cursor");
+var holes = [...document.querySelectorAll(".hole")];
+var score = 0;
+var scoreall = document.querySelector(".score span");
+var sound = new Audio("images/smash.mp3")
+
+
+
+function run(){
+    var i = Math.floor(Math.random() * holes.length)
+    var hole = holes[i];
+    var timer;
+    var img = document.createElement("img");
+    img.classList.add ("rabbit");
+    img.src = 'images/rabbit.png';
+
+    img.addEventListener("click", function(){
+        sound.play();
+        score += 10;
+        scoreall.textContent = score;
+        img.src = 'images/smash-rabbit.png';
+         
+    })
+
+hole.appendChild(img);
+timer =setTimeout(() => {
+    hole.removeChild(img);
+    run();
+}, 1500);
+}
+run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 window.addEventListener("mousemove", function(e){
     cursor.style.top = e.pageY + "px";
